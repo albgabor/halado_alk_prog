41d853368c3f641b91aa702ba5eb0723bde5e5a5
Albrecht Gabor <albgabor@login03.caesar.elte.hu>
2019-03-08 16:02:25 +0100

    Remove build dirs

--- ora_4/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/albgabor/halado_alk_prog/ora_4
-# Build directory: /home/albgabor/halado_alk_prog/ora_4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
